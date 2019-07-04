b1cacf0163072b262984d23da85294ac36a5d236
matheusbortolini <matheusbortolini@hotmail.com>
2019-07-04 10:26:47 -0300

    default geometry hard coded

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -1,56 +1,28 @@
 import React from 'react'
 
+const g = {
+  line_width: 10,
+  field_length: 6000,
+  field_width: 4000,
+  boundary_width: 250,
+  referee_width: 500,
+  goal_width: 700,
+  goal_depth: 180,
+  goal_wall_width: 20,
+  center_circle_radius: 500,
+  defense_radius: 500,
+  defense_stretch: 350,
+  free_kick_from_defense_dist: 700,
+  penalty_spot_from_field_line_dist: 450,
+  penalty_line_from_spot_dist: 350,
+}
+
 class Field extends React.Component {
   constructor(props) {
     super(props)
 
-    const fieldLength = 12000
-    const fieldWidth = 9000
-    const centerCircleRadius = 500
-
     this.state = {
       field: {
-        fieldWidth: fieldWidth,
-        fieldLength: fieldLength,
-        boundaryWidth: 300,
-        penAreaWidth: 1000,
-        penAreaDepth: 500,
-        goalWidth: 600,
-        goalDepth: 180,
-        centerCircleRadius: centerCircleRadius,
-        ballRadius: 21.5,
-        lines: [
-          {
-            p1: { x: -fieldLength / 2, y: -fieldWidth / 2 },
-            p2: { x: -fieldLength / 2, y: fieldWidth / 2 }
-          },
-          {
-            p1: { x: -fieldLength / 2, y: fieldWidth / 2 },
-            p2: { x: fieldLength / 2, y: fieldWidth / 2 }
-          },
-          {
-            p1: { x: fieldLength / 2, y: fieldWidth / 2 },
-            p2: { x: fieldLength / 2, y: -fieldWidth / 2 }
-          },
-          {
-            p1: { x: fieldLength / 2, y: -fieldWidth / 2 },
-            p2: { x: -fieldLength / 2, y: -fieldWidth / 2 }
-          },
-          {
-            p1: { x: 0, y: fieldWidth / 2 },
-            p2: { x: 0, y: -fieldWidth / 2 }
-          }
-        ],
-        circles: [
-          {
-            center: { x: 0, y: 0 },
-            radius: centerCircleRadius,
-            stroke: 'white',
-            strokeWidth: 10,
-            fill: '',
-            fillOpacity: 0
-          }
-        ],
         paths: [
           {
             d: [
@@ -85,17 +57,13 @@ class Field extends React.Component {
   }
 
   viewBox() {
-    if (this.rotateField) {
-      return (-(this.state.field.fieldWidth / 2 + this.state.field.boundaryWidth))
-        + ' ' + (-(this.state.field.fieldLength / 2 + this.state.field.boundaryWidth))
-        + ' ' + (this.state.field.fieldWidth + this.state.field.boundaryWidth * 2)
-        + ' ' + (this.state.field.fieldLength + this.state.field.boundaryWidth * 2)
-    }
+    const inner_width = 7500
+    const inner_height = 5500
 
-    return (-(this.state.field.fieldLength / 2 + this.state.field.boundaryWidth))
-      + ' ' + (-(this.state.field.fieldWidth / 2 + this.state.field.boundaryWidth))
-      + ' ' + (this.state.field.fieldLength + this.state.field.boundaryWidth * 2)
-      + ' ' + (this.state.field.fieldWidth + this.state.field.boundaryWidth * 2)
+    return -(inner_width / 2)
+      + ' ' + - (inner_height / 2)
+      + ' ' + inner_width
+      + ' ' + inner_height
   }
 
   pathFromD(pd) {
@@ -121,31 +89,6 @@ class Field extends React.Component {
   }
 
   render() {
-    const lines = this.state.field.lines.map((l, i) => {
-      return (
-        <line
-          key={"line-" + i}
-          x1={l.p1.x}
-          y1={l.p1.y}
-          x2={l.p2.x}
-          y2={l.p2.y}
-          className="field-line">
-        </line>
-      )
-    })
-
-    const circles = this.state.field.circles.map((c, i) => {
-      return (
-        <circle
-          key={"circle-" + i}
-          cx={c.center.x}
-          cy={c.center.y}
-          r={c.radius}
-          className="field-circle">
-        </circle>
-      )
-    })
-
     const paths = this.state.field.paths.map((p, i) => {
       return (
         <path
@@ -169,45 +112,125 @@ class Field extends React.Component {
       )
     })
 
+    let dField = ''
+    dField += `M ${-g.field_length / 2} ${-g.field_width / 2} h ${g.field_length} v ${g.field_width} h-${g.field_length} v-${g.field_width} m ${g.line_width} ${g.line_width} v ${g.field_width - 2 * g.line_width} h ${g.field_length - 2 * g.line_width} v-${g.field_width - 2 * g.line_width} h-${g.field_length - 2 * g.line_width}\n`
+
+    dField += `M ${g.line_width / 2}-${g.field_width / 2} v ${g.field_width} h-${g.line_width} v-${g.field_width} h ${g.line_width}\n`
+
+    dField += `M 0-${g.center_circle_radius} a ${g.center_circle_radius} ${g.center_circle_radius} 0 0 1 0 ${2 * g.center_circle_radius} a ${g.center_circle_radius} ${g.center_circle_radius} 0 0 1 0-${2 * g.center_circle_radius} m 0 ${g.line_width} a ${g.center_circle_radius - g.line_width} ${g.center_circle_radius - g.line_width} 0 0 0 0 ${2 * (g.center_circle_radius - g.line_width)} a ${g.center_circle_radius - g.line_width} ${g.center_circle_radius - g.line_width} 0 0 0 0-${2 * (g.center_circle_radius - g.line_width)}\n`
+
+    dField += `M 0-${1.5 * g.line_width} a ${1.5 * g.line_width} ${1.5 * g.line_width} 0 0 1 0 ${3 * g.line_width} a ${1.5 * g.line_width} ${1.5 * g.line_width} 0 0 1 0-${3 * g.line_width}\n`
+
+    dField += `M-${g.field_length / 2}-${g.defense_radius + g.defense_stretch / 2} a ${g.defense_radius} ${g.defense_radius} 0 0 1 ${g.defense_radius} ${g.defense_radius} v ${g.defense_stretch} a ${g.defense_radius} ${g.defense_radius} 0 0 1-${g.defense_radius} ${g.defense_radius} v-${g.line_width} a ${g.defense_radius - g.line_width} ${g.defense_radius - g.line_width} 0 0 0 ${g.defense_radius - g.line_width}-${g.defense_radius - g.line_width} v-${g.defense_stretch} a ${g.defense_radius - g.line_width} ${g.defense_radius - g.line_width} 0 0 0-${g.defense_radius - g.line_width}-${g.defense_radius - g.line_width}\n`
+
+    dField += `M ${g.field_length / 2} ${g.defense_radius + g.defense_stretch / 2} a ${g.defense_radius} ${g.defense_radius} 0 0 1-${g.defense_radius}-${g.defense_radius} v-${g.defense_stretch} a ${g.defense_radius} ${g.defense_radius} 0 0 1 ${g.defense_radius}-${g.defense_radius} v ${g.line_width} a ${g.defense_radius - g.line_width} ${g.defense_radius - g.line_width} 0 0 0-${g.defense_radius - g.line_width} ${g.defense_radius - g.line_width} v ${g.defense_stretch} a ${g.defense_radius - g.line_width} ${g.defense_radius - g.line_width} 0 0 0 ${g.defense_radius - g.line_width} ${g.defense_radius - g.line_width}\n`
+
+    dField += `M-${g.field_length / 2 - g.penalty_spot_from_field_line_dist - g.line_width} 0 a ${1 * g.line_width} ${1 * g.line_width} 0 0 1 ${2 * g.line_width} 0 a ${1 * g.line_width} ${1 * g.line_width} 0 0 1-${2 * g.line_width} 0\n`
+
+    dField += `M ${g.field_length / 2 - g.penalty_spot_from_field_line_dist - g.line_width} 0 a ${1 * g.line_width} ${1 * g.line_width} 0 0 1-${2 * g.line_width} 0 a ${1 * g.line_width} ${1 * g.line_width} 0 0 1 ${2 * g.line_width} 0\n`
+
+    dField += `z`
+
+    let dLeftGoal = `M-${g.field_length / 2}-${g.goal_width / 2 + g.goal_wall_width / 2}
+    h-${g.goal_depth + g.goal_wall_width}
+    v ${g.goal_width + 2 * g.goal_wall_width}
+    h ${g.goal_depth + g.goal_wall_width}
+    v-${g.goal_wall_width}
+    h-${g.goal_depth}
+    v-${g.goal_width}
+    h ${g.goal_depth}
+    z`
+
+    let dRightGoal = `M ${g.field_length / 2}-${g.goal_width / 2 + g.goal_wall_width / 2}
+    h ${g.goal_depth + g.goal_wall_width}
+    v ${g.goal_width + 2 * g.goal_wall_width}
+    h-${g.goal_depth + g.goal_wall_width}
+    v-${g.goal_wall_width}
+    h ${g.goal_depth}
+    v-${g.goal_width}
+    h-${g.goal_depth}
+    z`
+
     return (
       <React.Fragment>
-        <svg
-          ref="canvas"
-          viewBox={this.viewBox()}
-          className="field-canvas">
-          <rect
-            x={-(this.state.field.fieldLength / 2 + this.state.field.boundaryWidth)}
-            y={-(this.state.field.fieldWidth / 2 + this.state.field.boundaryWidth)}
-            width={this.state.field.fieldLength + this.state.field.boundaryWidth * 2}
-            height={this.state.field.fieldWidth + this.state.field.boundaryWidth * 2}
-            ref="background"
-            className="field-background">
-          </rect>
-          {lines}
-          {circles}
-          {paths}
-          {texts}
-        </svg>
+        <div className="field">
+          <svg
+            viewBox={this.viewBox()}
+            x="50%"
+            y="50%"
+            width="100%"
+            height="100%">
+            <path className="field-line" d={dField}></path>
+            <path className="left-goal" d={dLeftGoal}></path>
+            <path className="right-goal" d={dRightGoal}></path>
+            <text
+              className="time-left"
+              lengthAdjust="spacingAndGlyphs"
+              x="2"
+              y={-g.field_width / 2 - 25}>
+            </text>
+            <text
+              className="team-name left-name"
+              textAnchor="start"
+              lengthAdjust="spacingAndGlyphs"
+              x={-g.field_length / 2 + 25}
+              y={-g.field_width / 2 + 25 + 250}>
+            </text>
+            <text
+              className="team-name right-name"
+              textAnchor="end"
+              lengthAdjust="spacingAndGlyphs"
+              x={g.field_length / 2 - 25}
+              y={-g.field_width / 2 + 25 + 250}>
+            </text>
+            <text
+              className="team-name left-score"
+              textAnchor="end"
+              x={-25}
+              y={-g.field_width / 2 + 25 + 250}>
+            </text>
+            <text
+              className="team-name right-score"
+              textAnchor="start"
+              x={25}
+              y={-g.field_width / 2 + 25 + 250}>
+            </text>
+            {paths}
+            {texts}
+          </svg>
+        </div>
         <style jsx="true">{`
-          .field-canvas {
-            width: 100%;
-            height: 100%;
-            display: table-row;
+          .field {
+            background-color: #19770f;
           }
 
-          .field-background {
-            fill: green;
+          .field-line, .left-goal, right-goal, .field-path, .field-text {
+            stroke-width: 5;
             fill-opacity: 1;
-            stroke: none;
           }
 
-          .field-line, .field-circle, .field-path, .field-text {
-            stroke-width: 10;
+          .field-line {
             stroke: white;
-            fill-opacity: 1;
+            fill: white;
+          }
+
+          .left-goal {
+            stroke: #3276b1;
+            fill: #3276b1;
+          }
+
+          .right-goal {
+            fill: #ede528;
+            stroke: #d5c512;
+          }
+
+          .field-path {
+            stroke: white;
+            fill: #ede528;
           }
 
           .field-text {
+            fill: black;
             stroke-width: 0;
             text-anchor: middle;
             dominant-baseline: central;
